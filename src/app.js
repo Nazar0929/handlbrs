c57c3e6bad73a791f6acbaf7ed02a779d74be624
Nazar0929 <166243366+Nazar0929@users.noreply.github.com>
2025-10-18 15:27:25 +0300

    piush

--- src/app.js
+++ src/app.js
@@ -3,11 +3,12 @@ import templateSocials from "./socials-template.hbs"
 
 import bloggers from "./media.json"
 import templateBloggers from "./bloggers-template.hbs"
-import { log } from "handlebars/runtime";
-
 
 const listEl = document.querySelector(".list__socials");
 const listRef = document.querySelector(".list__bloggers");
 
+// Социальные сети
 listEl.innerHTML = templateSocials({socials});
+
+// Блогеры
 listRef.innerHTML = templateBloggers({bloggers});
